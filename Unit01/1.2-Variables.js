14c75ee16ebe8f99677e98031251a75f473fe729
GianniB13 <159183504+GianniB13@users.noreply.github.com>
2024-04-04 14:22:06 -0400

    1.1-1.3

---
+++ Unit01/1.2-Variables.js
@@ -0,0 +1,12 @@
+//part 1:
+length = 10;
+width = 5;
+area = length * width;
+console.log (area);
+console.log ("area"+ area);
+
+//part 2:
+first_name = "Santa";
+last_name = "Claws";
+last_name = "Claus";
+console.log ("I still believe in + first_name,last_name");
---
+++ Unit01/1.3-Math.js
@@ -0,0 +1,14 @@
+//part 1
+celcius = 100;
+fahrenhiet = 0;
+celcius = celcius + 2;
+fahrenheit = (celcius * 9/5) + 32;
+console.log(fahrenheit);
+
+//part 2
+fahrenheit = 77;
+celcius = (fahrenheit - 32) * 5/9;
+console.log(celcius);
+console.log("77 degrees Fahrenheit is",celcius,"degrees celcius.");
+
+//part 3
